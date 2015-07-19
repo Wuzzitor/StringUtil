5148919561666d82fe2e1d13260b50c5983dc21a
Matthias Molitor <mmo@webfactory.de>
2015-07-19 19:10:18 +0200

    added string util class from Matthimatiker/MolComponents

---
+++ src/String.php
@@ -0,0 +1,204 @@
+<?php
+
+/**
+ * Mol_Util_String
+ *
+ * @category PHP
+ * @package Mol_Util
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 21.06.2012
+ */
+
+namespace Wuzzitor\StringUtil;
+
+/**
+ * Contains helper methods for string handling.
+ *
+ * This class contains lightweight helper methods that simplify string handling.
+ * All operations are independent of the underlying charset of the subject string.
+ *
+ * If you need to perform actions that depend on the charset, then try to use
+ * Mol_DataType_String as it includes charset handling.
+ *
+ * Hint:
+ * The helper methods are static and require a string to operate on. That string
+ * is called "subject" and its always the first argument in all methods.
+ *
+ * @category PHP
+ * @package Mol_Util
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 21.06.2012
+ */
+class String
+{
+
+    /**
+     * Checks if the string starts with the provided prefix.
+     *
+     * @param string $subject The tested string.
+     * @param string $prefix
+     * @return boolean True if the string starts with the prefix, false otherwise.
+     */
+    public static function startsWith($subject, $prefix)
+    {
+        return strpos($subject, $prefix) === 0;
+    }
+
+    /**
+     * Checks if the string ends with the provided suffix.
+     *
+     * @param string $subject The tested string.
+     * @param string $suffix
+     * @return boolean True if the string ends with the suffix, false otherwise.
+     */
+    public static function endsWith($subject, $suffix)
+    {
+        $expectedPosition = strlen($subject) - strlen($suffix);
+        return strrpos($subject, $suffix) === $expectedPosition;
+    }
+
+    /**
+     * Checks if the string contains the provided needle.
+     *
+     * @param string $subject The tested string.
+     * @param string $needle
+     * @return boolean True if the string contains the needle, false otherwise.
+     */
+    public static function contains($subject, $needle)
+    {
+        return strpos($subject, (string)$needle) !== false;
+    }
+
+    /**
+     * Checks if the string contains any of the provided needles.
+     *
+     * @param string $subject The tested string.
+     * @param array(string) $needles
+     * @return boolean True if the string contains a needle, false otherwise.
+     */
+    public static function containsAny($subject, array $needles)
+    {
+        if (count($needles) === 0) {
+            return true;
+        }
+        foreach ($needles as $needle) {
+            /* @var $needle string */
+            if (self::contains($subject, $needle)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Checks if the string contains all of the provided needles.
+     *
+     * @param string $subject The tested string.
+     * @param array(string) $needles
+     * @return boolean True if the string contains all needles, false otherwise.
+     */
+    public static function containsAll($subject, array $needles)
+    {
+        foreach ($needles as $needle) {
+            /* @var $needle string */
+            if (!self::contains($subject, $needle)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Removes the given prefix from the string.
+     *
+     * This method has no effect if the string does not start with $prefix.
+     *
+     * @param string $subject
+     * @param string $prefix
+     * @return string String without prefix.
+     */
+    public static function removePrefix($subject, $prefix)
+    {
+        if (!self::startsWith($subject, $prefix)) {
+            // Nothing to remove.
+            return $subject;
+        }
+        return substr($subject, strlen($prefix));
+    }
+
+    /**
+     * Removes the given suffix from the string.
+     *
+     * This method has no effect if the string does not end with $suffix.
+     *
+     * @param string $subject
+     * @param string $suffix
+     * @return string String without suffix.
+     */
+    public static function removeSuffix($subject, $suffix)
+    {
+        if (!self::endsWith($subject, $suffix)) {
+            // Nothing to remove.
+            return $subject;
+        }
+        return substr($subject, 0, strlen($subject) - strlen($suffix));
+    }
+
+    /**
+     * Replaces all occurrences of $searchOrMapping by $replace.
+     *
+     * This method provides 3 signatures:
+     *
+     * replace(string, string, string):
+     *
+     *     $result = Mol_Util_String::replace('my string', 'search', 'replace');
+     *
+     * Replaces all occurrences of "search" by "replace".
+     *
+     * replace(string, array(string), string):
+     *
+     *     $needles = array(
+     *         'first',
+     *         'seconds'
+     *     );
+     *     $result = Mol_Util_String::replace('my string', $needles, 'replace');
+     *
+     * Replaces all string that are contained in the $needles array by "replace".
+     *
+     * replace(string, array(string=>string)):
+     *
+     *     $mapping = array(
+     *         'first' => 'last',
+     *         'hello' => 'world'
+     *     );
+     *     $result = Mol_Util_String::replace('my string', $mapping);
+     *
+     * Expects an associative array that represents a mapping of strings
+     * as argument.
+     * The keys are replaced by the assigned values.
+     * In this example occurrences of "first" are replaced by "last" and
+     * "hello" is replaced by "world".
+     *
+     * @param string $subject
+     * @param string|array(integer|string=>string) $searchOrMapping
+     * @param string $replace
+     * @return string The string with applied replacements.
+     */
+    public static function replace($subject, $searchOrMapping, $replace = null)
+    {
+        $search = $searchOrMapping;
+        if ($replace === null && is_array($searchOrMapping)) {
+            // Mapping provided.
+            $search  = array_keys($searchOrMapping);
+            $replace = array_values($searchOrMapping);
+        }
+        return str_replace($search, $replace, $subject);
+    }
+
+}
---
+++ tests/StringTest.php
@@ -0,0 +1,298 @@
+<?php
+
+/**
+ * StringTest
+ *
+ * @category PHP
+ * @package Mol_Util
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 21.06.2012
+ */
+
+namespace Wuzzitor\StringUtil;
+
+/**
+ * Tests the string helper class.
+ *
+ * @category PHP
+ * @package Mol_Util
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 21.06.2012
+ */
+class StringTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Ensures that startsWith() returns true if the string starts with the given
+     * prefix.
+     */
+    public function testStartsWithReturnsTrueIfTheStringStartsWithTheProvidedPrefix()
+    {
+        $result = String::startsWith('this is a test string', 'this');
+        $this->assertTrue($result);
+    }
+
+    /**
+     * Ensures that startsWith() returns false if the string does not start with
+     * the prefix but contains it.
+     */
+    public function testStartsWithReturnsFalseIfTheStringOnlyContainsThePrefix()
+    {
+        $result = String::startsWith('this is a test string', 'test');
+        $this->assertFalse($result);
+    }
+
+    /**
+     * Ensures that startsWith() returns false if the string does not even contain the prefix.
+     */
+    public function testStartsWithReturnsFalseIfTheStringDoesNotContainThePrefix()
+    {
+        $result = String::startsWith('this is a test string', 'demo');
+        $this->assertFalse($result);
+    }
+
+    /**
+     * Ensures that startsWith() returns false if the string is shorter than the
+     * prefix and equals the first part of the prefix.
+     */
+    public function testStartsWithReturnsFalseIfStringEqualsFirstPartOfPrefix()
+    {
+        $result = String::startsWith('test', 'testprefix');
+        $this->assertFalse($result);
+    }
+
+    /**
+     * Ensures that endsWith() returns true if the string ends with the given suffix.
+     */
+    public function testEndsWithReturnsTrueIfTheStringEndsWithTheProvidedSuffix()
+    {
+        $result = String::endsWith('this is a test string', 'string');
+        $this->assertTrue($result);
+    }
+
+    /**
+     * Ensures that endsWith() returns false if the string does not end with the
+     * given suffix but contains it.
+     */
+    public function testEndsWithReturnsFalseIfTheStringOnlyContainsTheSuffix()
+    {
+        $result = String::endsWith('this is a test string', 'test');
+        $this->assertFalse($result);
+    }
+
+    /**
+     * Ensures that endsWith() returns false if the string does not even contain
+     * the given suffix.
+     */
+    public function testEndsWithReturnsFalseIfTheStringDoesNotContainTheSuffix()
+    {
+        $result = String::endsWith('this is a test string', 'demo');
+        $this->assertFalse($result);
+    }
+
+    /**
+     * Ensures that contains() returns false if the string does not contain
+     * the needle.
+     */
+    public function testContainsReturnsFalseIfStringDoesNotContainNeedle()
+    {
+        $result = String::contains('abc', 'd');
+        $this->assertFalse($result);
+    }
+
+    /**
+     * Ensures that contains() returns true if the string contains the needle.
+     */
+    public function testContainsReturnsTrueIfStringContainsNeedle()
+    {
+        $result = String::contains('abc', 'b');
+        $this->assertTrue($result);
+    }
+
+    /**
+     * Ensures that contains() returns true if string and needle are equal.
+     */
+    public function testContainsReturnsTrueIfStringEqualsNeedle()
+    {
+        $result = String::contains('abc', 'abc');
+        $this->assertTrue($result);
+    }
+
+    /**
+     * Checks if contains() searches for digits that are passed as argument.
+     *
+     * Ensures that integer values are not converted to characters internally.
+     */
+    public function testContainsSearchesForProvidedDigits()
+    {
+        $result = String::contains('123', 2);
+        $this->assertTrue($result);
+    }
+
+    /**
+     * Ensures that containsAny() returns true if a list of needles is provided and
+     * the string contains at least one needle in the list.
+     */
+    public function testContainsAnyReturnsTrueIfStringContainsAtLeastOneOfTheNeedles()
+    {
+        $result = String::containsAny('abc', array('d', 'a'));
+        $this->assertTrue($result);
+    }
+
+    /**
+     * Ensures that containsAny() returns false if a list of needles is provided
+     * and the string does not contain any of the needles.
+     */
+    public function testContainsAnyReturnsFalseIfStringContainsNoneOfTheNeedles()
+    {
+        $result = String::containsAny('abc', array('d', 'f'));
+        $this->assertFalse($result);
+    }
+
+    /**
+     * Ensures that containsAny() returns true if an empty list of needles is provided.
+     */
+    public function testContainsAnyReturnsTrueIfListOfNeedlesIsEmpty()
+    {
+        $result = String::containsAny('abc', array());
+        $this->assertTrue($result);
+    }
+
+    /**
+     * Ensures that containsAll() returns true if the string contains all
+     * of the provided needles.
+     */
+    public function testContainsAllReturnsTrueIfStringContainsAllNeedles()
+    {
+        $result = String::containsAll('abc', array('a', 'c'));
+        $this->assertTrue($result);
+    }
+
+    /**
+     * Ensures that containsAll() returns false if the string contains only some
+     * of the provided needles.
+     */
+    public function testContainsAllReturnsFalseIfStringContainsOnlySomeOfTheNeedles()
+    {
+        $result = String::containsAll('abc', array('a', 'd', 'c'));
+        $this->assertFalse($result);
+    }
+
+    /**
+     * Ensures that containsAll() returns true if an empty list of needles is provided.
+     */
+    public function testContainsAllReturnsTrueIfListOfNeedlesIsEmpty()
+    {
+        $result = String::containsAll('abc', array());
+        $this->assertTrue($result);
+    }
+
+    /**
+     * Checks if removePrefix() removes the given prefix from the string.
+     */
+    public function testRemovePrefixRemovesProvidedPrefix()
+    {
+        $result = String::removePrefix('this is a test string', 'this ');
+        $this->assertEquals('is a test string', $result);
+    }
+
+    /**
+     * Ensures that removePrefix() removes the prefix only once.
+     */
+    public function testRemovePrefixRemovesPrefixOnlyOnce()
+    {
+        $result = String::removePrefix('testtestdemo', 'test');
+        $this->assertEquals('testdemo', $result);
+    }
+
+    /**
+     * Ensures that removePrefix() does not modify the string if it does not
+     * start with the prefix but contains it.
+     */
+    public function testRemovePrefixDoesNotModifyStringIfItOnlyContainsPrefix()
+    {
+        $result = String::removePrefix('this is a test string', 'test ');
+        $this->assertEquals('this is a test string', $result);
+    }
+
+    /**
+     * Checks if removeSuffix() removes the provided suffix from the string.
+     */
+    public function testRemoveSuffixRemovesProvidedSuffix()
+    {
+        $result = String::removeSuffix('this is a test string', ' string');
+        $this->assertEquals('this is a test', $result);
+    }
+
+    /**
+     * Ensures that removeSuffix() removes the suffix only once.
+     */
+    public function testRemoveSuffixRemovesSuffixOnlyOnce()
+    {
+        $result = String::removeSuffix('demotesttest', 'test');
+        $this->assertEquals('demotest', $result);
+    }
+
+    /**
+     * Ensures that removeSuffix() does not modify the string if it does not
+     * end with the suffix but contains it.
+     */
+    public function testRemoveSuffixDoesNotModifyStringIfItOnlyContainsSuffix()
+    {
+        $result = String::removeSuffix('this is a test string', 'test');
+        $this->assertEquals('this is a test string', $result);
+    }
+
+    /**
+     * Ensures that replace() does not modify the string if it does not contain
+     * the search value.
+     */
+    public function testReplaceDoesNotModifyStringIfItDoesNotContainSearchString()
+    {
+        $result = String::replace('hello world', 'foo', 'bar');
+        $this->assertEquals('hello world', $result);
+    }
+
+    /**
+     * Tests signature replace(string, string, string):
+     * Checks if replace() replaces the search string by the provided values.
+     */
+    public function testReplaceReplacesSingleSearchStringByReplaceValue()
+    {
+        $result = String::replace('hello world', 'hello', 'bye');
+        $this->assertEquals('bye world', $result);
+    }
+
+    /**
+     * Tests signature replace(string, array(string), string):
+     * Checks if replace() replaces all search strings by the provided value.
+     */
+    public function testReplaceReplacesListOfSearchStringsByReplaceValue()
+    {
+        $result = String::replace('hello world', array('hello', 'world'), 'dummy');
+        $this->assertEquals('dummy dummy', $result);
+    }
+
+    /**
+     * Tests signature replace(string, array(string=>string)):
+     * Checks if replace() applies the mapping of search/replace pairs to the string.
+     */
+    public function testReplaceAppliesMappingIfAssociativeArrayIsProvided()
+    {
+        $mapping = array(
+                'hello' => 'welcome',
+                'world' => 'home'
+        );
+        $result  = String::replace('hello world', $mapping);
+        $this->assertEquals('welcome home', $result);
+    }
+
+}
